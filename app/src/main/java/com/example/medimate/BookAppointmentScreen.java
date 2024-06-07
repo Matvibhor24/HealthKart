46f95d80be2666d5bb076fb2210a2819f489b526
Vibhor Mathur <matvibhor24@gmail.com>
2024-06-07 22:12:04 +0530

    All doctors screen + Book Appointment screen

---
+++ app/src/main/java/com/example/medimate/BookAppointmentScreen.java
@@ -0,0 +1,20 @@
+package com.example.medimate;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BookAppointmentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_book_appointment_screen);
+
+    }
+}
